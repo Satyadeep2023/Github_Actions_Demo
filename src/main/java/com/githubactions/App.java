ba59e8a512a88109bb5eb53b6e0b792166f4f1df
Satyadeep2023 <125433274+Satyadeep2023@users.noreply.github.com>
2024-01-29 22:23:36 +0530

    Update App.java

--- src/main/java/com/githubactions/App.java
+++ src/main/java/com/githubactions/App.java
@@ -4,7 +4,7 @@ package com.githubactions;
  * Hello world!
  *
  */
-public class Demo  
+public class App  
 {
     public static void main( String[] args )
     { // two integer variables with values
