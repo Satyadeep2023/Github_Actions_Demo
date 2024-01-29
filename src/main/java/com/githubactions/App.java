049535f72d28f5adc82a695bdd44482e2e99ada7
satyadeep <Satyadeep.Bhardwaj@kyndryl.com>
2024-01-29 14:20:17 +0530

    Modified

--- src/main/java/com/githubactions/App.java
+++ src/main/java/com/githubactions/App.java
@@ -4,7 +4,7 @@ package com.githubactions;
  * Hello world!
  *
  */
-public class App 
+public class App  
 {
     public static void main( String[] args )
     { // two integer variables with values
