7395d8f52afab6aa223f295d6521f6fcf33c2fb4
satyadeep <Satyadeep.Bhardwaj@kyndryl.com>
2024-01-29 13:20:32 +0530

    Demo for githubactions

---
+++ src/main/java/com/githubactions/App.java
@@ -0,0 +1,24 @@
+package com.githubactions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    { // two integer variables with values
+        // and a variable "sum" to store the result
+    
+        int num1 = 200, num2 = 150,sum;
+       
+    
+    
+        //calculating the sum of num1 and num2 and
+        //storing the result in the variable sum
+        sum = num1+num2;
+    
+        //printing the result
+        System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
+    }
+}
